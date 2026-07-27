d943b983737b5d91544569d21bd76ca6ddb5b347
Arkady <arkgaranin@gmail.com>
2026-07-28 00:15:58 +0300

    ДЗ 3 и 4: Перешел с ОК на тест сайт. Подключил allure к тестам. Написал тесты: test_empty_password и test_empty_login

--- core/BaseTest.py
+++ core/BaseTest.py
@@ -1,14 +1,9 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 
 
 @pytest.fixture(scope='session')
 def browser():
-    driver_path = r"C:\Users\Arkady\chrome-webdriver\yandex\yandexdriver.exe"
-    service = Service(executable_path=driver_path)
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=service, options=options)
-    # driver = webdriver.Chrome() # в тест-хроме почему-то нет некоторых кнопок на сайте ОК
+    driver = webdriver.Chrome()
     yield driver
     driver.quit()
--- pages/BasePage.py
+++ pages/BasePage.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
+import allure
 
 
 class BasePage:
@@ -10,5 +11,9 @@ class BasePage:
         return WebDriverWait(self.driver, time).until(expected_conditions.visibility_of_element_located(locator),
                                                       message=f"Не удалось найти элеменет{locator}")
 
+    @allure.step('Открываем страницу')
     def get_url(self, url):
         return self.driver.get(url)
+
+    def attach_screenschot(self):
+        allure.attach(self.driver.get_screenshot_as_png(), "скриншот", allure.attachment_type.PNG)
